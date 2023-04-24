4188439103a28ea415c0e28f595adf6a8e71f345
Gabriel Anceski <gabrielanceski@gmail.com>
2023-04-24 12:04:22 -0300

    first commit

---
+++ src/main/java/br/edu/ifrs/gabrielanceski/techrepo/TechrepoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.gabrielanceski.techrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechrepoApplication.class, args);
+	}
+
+}
