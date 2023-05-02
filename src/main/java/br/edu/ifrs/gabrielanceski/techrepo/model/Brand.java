7c9ece83f94694122b71c90fd3a1a1e0da2d1f75
Gabriel Anceski <gabrielanceski@gmail.com>
2023-05-02 19:39:58 -0300

    fix: removed image relation

--- src/main/java/br/edu/ifrs/gabrielanceski/techrepo/model/Brand.java
+++ src/main/java/br/edu/ifrs/gabrielanceski/techrepo/model/Brand.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,8 +17,4 @@ public class Brand {
     private long id;
     @Column(name = "name", nullable = false, unique = true, length = 32)
     private String name;
-
-    @OneToOne
-    @JoinColumn(name = "image_id", nullable = true)
-    private Image image;
 }
