4801ba8b32f0a59018ccec5da25fd97ec5b37d7e
Gabriel Anceski <gabrielanceski@gmail.com>
2023-06-24 19:39:52 -0300

    fix: changed update annotation to @PatchMapping

--- src/main/java/br/edu/ifrs/gabrielanceski/techrepo/controller/BrandController.java
+++ src/main/java/br/edu/ifrs/gabrielanceski/techrepo/controller/BrandController.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +52,7 @@ public class BrandController {
         return ResponseEntity.ok(brand);
     }
 
-    @PutMapping("{brandId}")
+    @PatchMapping("{brandId}")
     public ResponseEntity<?> updateBrand(@PathVariable("brandId") Long brandId, @RequestBody @Valid BrandDTO brandDTO) {
         Brand brand = brandService.update(brandId, brandDTO);
         return ResponseEntity.ok(brand);
--- src/main/java/br/edu/ifrs/gabrielanceski/techrepo/controller/DeviceController.java
+++ src/main/java/br/edu/ifrs/gabrielanceski/techrepo/controller/DeviceController.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,7 +58,7 @@ public class DeviceController {
         return ResponseEntity.ok(device);
     }
 
-    @PutMapping("{deviceId}")
+    @PatchMapping("{deviceId}")
     public ResponseEntity<Device> update(@PathVariable("deviceId") Long id, @RequestBody @Valid DeviceDTO deviceDTO) {
         Device device = deviceService.update(id, deviceDTO);
         return ResponseEntity.ok(device);
