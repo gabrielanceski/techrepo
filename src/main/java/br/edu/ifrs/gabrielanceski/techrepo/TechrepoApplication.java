085442e68fec506c4d6917fd2f4e5b4d8d0f4e9e
Gabriel Anceski <gabrielanceski@gmail.com>
2023-07-09 20:20:32 -0300

    fix: jpa auditing enabled

--- src/main/java/br/edu/ifrs/gabrielanceski/techrepo/TechrepoApplication.java
+++ src/main/java/br/edu/ifrs/gabrielanceski/techrepo/TechrepoApplication.java
@@ -2,8 +2,10 @@ package br.edu.ifrs.gabrielanceski.techrepo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TechrepoApplication {
 
 	public static void main(String[] args) {
