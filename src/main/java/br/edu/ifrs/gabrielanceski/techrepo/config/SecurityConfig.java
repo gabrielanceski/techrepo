3c42c3991373e14a76d8cf414cdd2739b61d48e7
Gabriel Anceski <gabrielanceski@gmail.com>
2023-07-10 19:49:49 -0300

    fix: cors configuration

--- src/main/java/br/edu/ifrs/gabrielanceski/techrepo/config/SecurityConfig.java
+++ src/main/java/br/edu/ifrs/gabrielanceski/techrepo/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package br.edu.ifrs.gabrielanceski.techrepo.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.edu.ifrs.gabrielanceski.techrepo.jwt.JwtAuthenticationFilter;
 import br.edu.ifrs.gabrielanceski.techrepo.service.UserService;
@@ -53,6 +58,8 @@ public class SecurityConfig {
         http
             .csrf()
             .disable()
+            .cors()
+            .and()
             .authorizeHttpRequests()
             .requestMatchers("/api/v1/auth/**")
             .permitAll()
@@ -65,6 +72,19 @@ public class SecurityConfig {
             .authenticationProvider(authenticationProvider())
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
     }
 
 }
