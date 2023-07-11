0fae66486bf336cd3d5e106de9eda86135c2cd2b
Gabriel Anceski <gabrielanceski@gmail.com>
2023-07-10 21:18:12 -0300

    fix: cross origin auth

--- src/main/java/br/edu/ifrs/gabrielanceski/techrepo/controller/AuthController.java
+++ src/main/java/br/edu/ifrs/gabrielanceski/techrepo/controller/AuthController.java
@@ -2,6 +2,7 @@ package br.edu.ifrs.gabrielanceski.techrepo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import br.edu.ifrs.gabrielanceski.techrepo.service.AuthenticationService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/v1/auth")
 public class AuthController {
     private final AuthenticationService authService;
